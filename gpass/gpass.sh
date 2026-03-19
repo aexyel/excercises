d854fc1f6513937cc3b9dd8534e20f45c63c431a
Alexey V. Proschenko <alexey@proschenko.ru>
2026-03-19 12:25:27 +0300

    update search options

--- gpass/gpass.sh
+++ gpass/gpass.sh
@@ -1,5 +1,20 @@
 #!/bin/ksh
 
+# _list file [pattern]
+_list(){
+if [ "$2" = "" ]; then
+    gpg -d < $1 2> /dev/null | cut -f 1 -d ' '
+else
+    gpg -d < $1 2> /dev/null | cut -f 1 -d ' ' | fgrep -i $2
+fi
+}
+
+# _show file key
+_show(){
+gpg -d < $1 2> /dev/null | fgrep -w $2 | cut -f 2 -d ' ' 
+}
+
+
 ## gpg -c -a >file - вводишь пароль и хранимые данные
 ## gpg -d < file | fgrep -i xxxx | cut -f 2 - вводишь пароль, ищешь нужную строчку
 
@@ -12,6 +27,6 @@ if [ "$1" = "" ];
 fi
 
 case "$1" in
--l) gpg -d < $_file 2> /dev/null | cut -f 1 -d ' ' ;;
-*) gpg -d < $_file 2> /dev/null | fgrep -i $1 | cut -f 2 -d ' ' ;;
+-l) _list $_file $2 ;;
+*)  _show $_file $1 ;;
 esac
